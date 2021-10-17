e7da1cd1d5a90bcdfce19a2f0443b45846df9790
Jobayer Hossain <jobayerhossain977@gmail.com>
2021-10-17 22:51:54 +0600

    master: Added

--- container/src/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import { mount as ProductMount } from "products/ProductsIndex"
-import { mount as cartMount } from "cart/cartShow"
-
-ProductMount(document.querySelector("#my-products"))
-cartMount(document.querySelector("#my-cart"))
