256465fe1b93ce81e97a254ba9269d8cb75529c3
Jobayer Hossain <jobayerhossain977@gmail.com>
2021-10-19 14:21:24 +0600

    master: Added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,6 @@ import MarketingApp from "./components/MarketingApp"
 import React from "react"
 
 export default () => {
-	console.log("Hi there")
+	// console.log("Hi there")
 	return <MarketingApp />
 }
