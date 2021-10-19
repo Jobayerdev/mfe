439fba868984c65a23334c06f1f7d1d115c120e0
Jobayer Hossain <jobayerhossain977@gmail.com>
2021-10-19 21:31:09 +0600

    master: Added In React

--- auth/config/webpack.dev.js
+++ auth/config/webpack.dev.js
@@ -1,22 +1,22 @@
-const { merge } = require("webpack-merge")
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
-
-const commonConfig = require("./webpack.common")
 const packageJson = require("../package.json")
+const { merge } = require("webpack-merge")
+const commonConfig = require("./webpack.common")
 
 const devConfig = {
 	mode: "development",
 	devServer: {
-		port: 8080,
+		port: 8081,
 		historyApiFallback: {
 			index: "index.html",
 		},
 	},
 	plugins: [
 		new ModuleFederationPlugin({
-			name: "container",
-			remotes: {
-				marketing: "marketing@http://localhost:8081/remoteEntry.js",
+			name: "auth",
+			filename: "remoteEntry.js",
+			exposes: {
+				"./AuthApp": "./src/bootstrap",
 			},
 			shared: packageJson.dependencies,
 		}),
---
+++ auth/src/bootstrap.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const devRoot = document.querySelector("#_dev_auth")
+
+const d = <h1>Hello</h1>
+ReactDOM.render(d, devRoot)
