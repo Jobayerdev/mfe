23c42f7159c3cb38c5a2e92e8f794efff5362e33
Jobayerdev <jobayerhossain977@gmail.com>
2022-03-29 00:20:41 +0600

    ng: Added

--- user/src/app/app.component.spec.ts
+++ user/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'users'`, () => {
+  it(`should have as title 'user'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('users');
+    expect(app.title).toEqual('user');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('users app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('user app is running!');
   });
 });
